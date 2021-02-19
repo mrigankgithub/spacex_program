aff5d3d01b73b13ff6f94ba2afacf1af5b5269c5
mrigankgithub <kittu3217gmail.com>
2021-02-19 11:03:40 +0530

    final-release

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + './dist/space-x'));
+app.use(express.static('./dist/space-x'));
 
 app.get('/*', (req, res) =>
-res.sendFile(path.join(__dirname+'./dist/space-x/index.html'))
+    res.sendFile('index.html', {root: 'dist/space-x/'}),
 );
 
 // Start the app by listening on the default Heroku port
